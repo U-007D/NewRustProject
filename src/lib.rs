31e335995af1f7f18a100d5d2cabc45a76b200a2
bRad <brad@humanenginuity.com>
2017-08-05 16:08:21 -0700

    Rust Quickstart Template v3

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,22 @@
 #![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
 #![deny(unused_must_use, overflowing_literals)]
 
+extern crate fluent_validator;
+#[allow(unused_imports)]
+
 mod consts;
+mod args;
+mod error;
+#[cfg(test)] mod unit_tests;
 
-#[cfg(test)]
-mod unit_tests;
+use fluent_validator::FluentValidator;
+pub use consts::*;
+pub use error::Error;
+pub use args::Args;
 
-pub fn run(_args: Vec<String>) -> Result<(), Box<std::error::Error>>
-{
-    println!("Hello, {}-bit world!", 0usize.count_zeros());
+#[allow(unused_variables)]
+pub fn run(args: Args) -> Result<(), Error> {
+    args.clone().validate::<Args>()?;   //TODO: Make fluent_validator::validate() non-consuming
+    println!("Hello, ()-bit world!", 0usize.count_zeros());
     Ok(())
 }
